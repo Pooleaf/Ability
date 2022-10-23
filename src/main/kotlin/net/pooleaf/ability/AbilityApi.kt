3fdb4041194bbb032fb3cb92a3fcde2d3775f885
0uh1004 <0uh1004@gmail.com>
2022-10-24 01:20:13 +0900

    1. AbilityApi object로 수정

--- src/main/kotlin/net/pooleaf/ability/AbilityApi.kt
+++ src/main/kotlin/net/pooleaf/ability/AbilityApi.kt
@@ -3,14 +3,10 @@ package net.pooleaf.ability
 import net.pooleaf.ability.game.AbilityGame
 import net.pooleaf.ability.player.AbilityPlayerManager
 
-class AbilityApi {
+object AbilityApi {
 
-    companion object {
+    val game: AbilityGame = AbilityGame()
 
-        val game: AbilityGame = AbilityGame()
-
-        val playerManager: AbilityPlayerManager = AbilityPlayerManager()
-
-    }
+    val playerManager: AbilityPlayerManager = AbilityPlayerManager()
 
 }
