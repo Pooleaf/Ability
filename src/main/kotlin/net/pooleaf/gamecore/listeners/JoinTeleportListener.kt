9b8b163bb5f51459830de6f6b1a819b8e4474820
0uh1004 <0uh1004@gmail.com>
2022-10-24 20:21:08 +0900

    1. 관전자 접속 위치 이상한 오류 수정

--- src/main/kotlin/net/pooleaf/gamecore/listeners/JoinTeleportListener.kt
+++ src/main/kotlin/net/pooleaf/gamecore/listeners/JoinTeleportListener.kt
@@ -4,6 +4,7 @@ import net.pooleaf.core.modules.support.bukkit.util.TeleportUtil
 import net.pooleaf.core.modules.support.common.logger.Logger
 import net.pooleaf.gamecore.GameCore
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
@@ -12,17 +13,21 @@ class JoinTeleportListener: Listener {
     /**
      * 접속 시 상황에 맞는 장소로 텔레포트시킵니다.
      */
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
         val gamePlayer = GameCore.playerManager.get(player.uniqueId)
+
+        Logger.log("${GameCore.game.mapTeleported} / ${gamePlayer.observer}")
 
         when {
             // 대기 중 스폰
             !GameCore.game.mapTeleported && GameCore.spawnConfig.spawnLocation != null -> TeleportUtil.teleport(player, GameCore.spawnConfig.spawnLocation)
 
             // 관전자 게임 시작 후엔 맵으로
-            GameCore.game.mapTeleported && gamePlayer.observer -> TeleportUtil.teleport(player, GameCore.game.map!!.getCenterLocation())
+            GameCore.game.mapTeleported && gamePlayer.observer -> {
+                TeleportUtil.teleport(player, GameCore.game.map!!.getCenterLocation())
+            }
 
             // 게임 중인 플레이어 텔레포트 안됐으면 팀 스폰으로
             GameCore.game.mapTeleported && gamePlayer.isPlaying() && !GameCore.game.map!!.isInRadius(player.location) -> {
